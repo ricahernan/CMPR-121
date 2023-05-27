4d6401d87bfabc28d2fe463f23213122ea4126e0
ricahernan <119152847+ricahernan@users.noreply.github.com>
2023-05-26 21:39:44 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,1 +1,22 @@
+#pragma once
+#ifndef NODE_H
+#define NODE_H
+#include "CandidateList.h"
 
+class Node
+{
+public:
+	Node() : link(nullptr) {}
+	Node(const CandidateType& votes, Node* theLink)
+		: candidate(votes), link(theLink) {}
+	Node* getLink() const { return link; }
+	CandidateType getCandidate() const { return candidate; }
+	void setCandidate(const CandidateType& votes) { candidate = votes; }
+	void setLink(Node* theLink) { link = theLink; }
+	~Node() {}
+private:
+	CandidateType candidate;
+	Node* link; //pointer that points to next node
+};
+
+#endif
