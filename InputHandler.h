94c8d5b4a75bccf8ec34094df382f394689ffc66
ricahernan <119152847+ricahernan@users.noreply.github.com>
2023-05-26 21:38:57 -0700

    Update InputHandler.h

--- InputHandler.h
+++ InputHandler.h
@@ -1,1 +1,60 @@
+#pragma once
+#include <fstream>
+#include <string>
+#include "PersonType.h"
+#include "CandidateList.h"
 
+void createCandidateList(ifstream& infile, CandidateList& candidateList)
+{
+	int ssn = 0;
+	int allVotes[NUM_OF_CAMPUSES];
+	string fName, lName;
+
+	infile >> ssn;
+
+	while (ssn != -999)
+	{
+		CandidateType newCandidate;
+		infile >> fName;
+		infile >> lName;
+		newCandidate.setPersonInfo(fName, lName, ssn);
+		for (int i = 0; i < NUM_OF_CAMPUSES; ++i)
+		{
+			infile >> allVotes[i];
+			newCandidate.updateVotesByCampus(i, allVotes[i]);
+		}
+		candidateList.addCandidate(newCandidate);
+		infile >> ssn;
+	}
+}
+
+void readCandidateData(CandidateList& candidateList)
+{
+	ifstream infile;
+	infile.open("Candidate_data.txt");
+	if (!infile)
+	{
+		cerr << "Input file does not exist." << endl;
+		exit(1);
+	}
+
+	int ssn;
+	string firstName, lastName;
+	int votes[NUM_OF_CAMPUSES];
+
+	while (infile >> ssn >> firstName >> lastName)
+	{
+		CandidateType candidate;
+		candidate.setPersonInfo(firstName, lastName, ssn);
+
+		for (int i = 0; i < NUM_OF_CAMPUSES; ++i)
+		{
+			infile >> votes[i];
+			candidate.updateVotesByCampus(i + 1, votes[i]);
+		}
+
+		candidateList.addCandidate(candidate);
+	}
+
+	infile.close();
+}
